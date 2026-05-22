f8043023b4ae39a1a9c2aec38d5c88f9aacc5907
Nikolay Nedelchev <ndn@Nikolays-MacBook-Pro.local>
2026-05-22 08:45:40 +0300

    Remove old files from v2

---
+++ cpu_v2/include/qe6502/qe6502_abi.h
@@ -0,0 +1,18 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Nikolay Nedelchev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ */
+
+#ifndef QE6502_H
+#define QE6502_H
+
+#endif // QE6502_H
