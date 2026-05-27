3e2c327bac42f6ef4239d7c47dab8215cc4835fe
Nikolay Nedelchev <ndn@Nikolays-MacBook-Pro.local>
2026-05-27 21:54:58 +0300

    cleanup debugger

--- harness/transistor_netlist_test/debugger/main.cpp
+++ harness/transistor_netlist_test/debugger/main.cpp
@@ -1,14 +1,6 @@
 #include "command_loop.hpp"
 
 #include <cstdio>
-#include <string>
-
-namespace perfect6502_debug
-{
-
-int run_illegal_opcode_investigator(const char* executable_path);
-
-} // namespace perfect6502_debug
 
 int main(int argc, char** argv)
 {
@@ -17,13 +9,8 @@ int main(int argc, char** argv)
         return perfect6502_debug::run_command_loop();
     }
 
-    if ((argc == 2) && (std::string(argv[1]) == "investigate-illegal"))
-    {
-        return perfect6502_debug::run_illegal_opcode_investigator(argv[0]);
-    }
-
     (void)std::fprintf(stderr,
-                       "usage: %s [investigate-illegal]\n",
+                       "usage: %s\n",
                        (argc > 0) ? argv[0] : "perfect6502_debug");
     return 1;
 }
