3491d01b196ef8d2d250ddd18e508617bddc42b3
Nikolay Nedelchev <ndn@Nikolays-MacBook-Pro.local>
2026-05-28 16:09:53 +0300

    lost prefetch

--- cpu_v2/src/qe6502_v2.c
+++ cpu_v2/src/qe6502_v2.c
@@ -842,6 +842,7 @@ static qe6502_tick_t mc_brk_c5_vec_hi(qe6502_t* cpu, uint8_t bus)
 {
     cpu->PC = u16_set_byte(cpu->PC, 0, bus);
     cpu->P = (uint8_t)(cpu->P | flag_I);
+    prefetch(cpu);
     cpu->interrupts = (uint8_t)(cpu->interrupts & (~qe6502_interrupt_accepted_nmi));
     return read(cpu, 0xffffu);
 }
